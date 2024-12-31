d494b4bc7b4f2ccfa1865a3079c51a89c5382840
Liu Wenyuan <15816141883@163.com>
2025-01-01 00:33:05 +0800

    Update snap_to_bgm_lut.py

--- snap_to_bpm_lut.py
+++ snap_to_bpm_lut.py
@@ -2,7 +2,10 @@
 
 SNAP_TO_BGM_LUT = {
     R"E:\joshw\ut_dusttale_sp\act1\02. 弄好事情 (Set something right) [BV1Cr421M7pA].m4a": 53,
+    R"E:\joshw\ut_dusttale_sp\act1\03. 你畏惧了的朋友 (Your scared friend) [BV1QK421k7jG].m4a": -100,
     R"E:\joshw\ut_dusttale_sp\act1\04. 敌军士气低落 (Enemies' low morale) (v2) [BV15f421i73M].m4a": (91.88, True),
+    #R"E:\joshw\ut_dusttale_sp\act1\07. 不归路 (No way back) [BV15A4m1G7na].m4a": 61.25,
+    R"E:\joshw\ut_dusttale_sp\act1\12. 惑之不解 (Perplexe) [BV1WW421N7Su].m4a": (145.066, True),
     R"E:\joshw\ut_dusttale_sp\act1\15. 瞎操心，结果尴尬了 (Awkworried) (v2) [BV1zr421M7U6].m4a": -100,
     R"E:\joshw\ut_dusttale_sp\act1\35a. 尸遗 (Corpsolate) (v1) [BV1Di421h7be].m4a": 86.15,
     #R"E:\joshw\ut_dusttale_sp\act1\36a. 归冢 (Vania) [BV14YWBeMErw].m4a": 96,
