7bd6d577054f6c834719d7201208fe93dc74cf04
Liu Wenyuan <15816141883@163.com>
2024-12-31 22:55:42 +0800

    Introduce BPM snapping and support only stripping start silence

--- __main__.py
+++ __main__.py
@@ -1,40 +1,75 @@
-from math import ceil
+from math import ceil, floor
 from silence import detect_leading_silence, detect_ending_silence
 from vgmstream_file import seg_from_vgmstream
-from os import path
+from os import path, makedirs
 from glob import iglob
 from sys import argv
+from snap_to_bpm_lut import SNAP_TO_BGM_LUT
 
 
-SILENCE_THRESOLD = -40 # dBFS
+SILENCE_THRESOLD = -36 # dBFS
+# For use with BPM estimation in FL Studio
+STRIP_START_ONLY = False
 
+
+def ms_to_samples(ms, frame_rate):
+    # This should absolutely return a int, otherwise vgmstream will interpret the number as seconds
+    return ceil(ms / 1000 * frame_rate)
+
+def snap_to_bpm(ms, bpm):
+    """Snap a given time in milliseconds to the nearest beat based on the specified BPM"""
+    if bpm <= 0: return ms
+    beat_length = (1 * 60 * 1000) / bpm
+    return ceil(ms / beat_length) * beat_length
 
 if len(argv) > 1:
     work_dir = argv[1]
 else:
     work_dir = input("work dir : ")
 work_dir = path.relpath(work_dir)
+out_dir = work_dir
+
+if STRIP_START_ONLY:
+    out_dir = path.join(work_dir, "silence_start")
+    makedirs(out_dir, exist_ok=True)
 
 
 for sound_path in iglob(path.join(work_dir, "*")):
-    sound_nameparts = path.splitext(sound_path)
-    if sound_nameparts[1] not in [".m4a", ".mp3"]:
+    sound_name_parts = path.splitext(path.basename(sound_path))
+    if sound_name_parts[1] not in [".m4a", ".mp3"]:
         continue
-    print(sound_path)
 
-    sound_name = sound_nameparts[0]
-    sound_txtp_path = sound_name + ".txtp"
+    sound_name = sound_name_parts[0]
+    sound_txtp_path = path.join(out_dir, sound_name + ".txtp")
     if path.exists(sound_txtp_path):
-        print(f"\t{sound_txtp_path} already exists")
         continue
+    else:
+        print(sound_path)
+
+    sound_bpm = SNAP_TO_BGM_LUT.get(path.realpath(sound_path), -1)
 
     sound = seg_from_vgmstream(sound_path)
-    def ms_to_samples(ms):
-        return ceil(ms / 1000 * sound.frame_rate)
-    lead_trim = ms_to_samples(detect_leading_silence(sound, silence_threshold=SILENCE_THRESOLD, chunk_size=1))
-    end_trim = ms_to_samples(detect_ending_silence(sound, silence_threshold=SILENCE_THRESOLD, chunk_size=1))
+    lead_trim = ms_to_samples(
+        detect_leading_silence(sound, silence_threshold=SILENCE_THRESOLD, chunk_size=1),
+        frame_rate=sound.frame_rate
+    )
+    strip_start_only_here = STRIP_START_ONLY or sound_bpm == -100
+    if not strip_start_only_here:
+        end_trim = ms_to_samples(
+            min(snap_to_bpm(
+                detect_ending_silence(sound, silence_threshold=SILENCE_THRESOLD, chunk_size=1),
+                bpm=sound_bpm
+            ), sound.duration_seconds * 1000),
+            frame_rate=sound.frame_rate
+        )
 
-    commands = f"#r {lead_trim} #t {end_trim}"
+    commands = f"#r {lead_trim}"
+    if not strip_start_only_here:
+        commands += f" #t {end_trim}"
     print(f"\t{commands}")
     with open(sound_txtp_path, "w", encoding="utf-8-sig") as sound_txtp:
-        sound_txtp.write(path.relpath(sound_path, start=work_dir) + " " + commands)
+        sound_txtp.write("# AUTO-GENERATED with batch_silence_detection.")
+        if sound_bpm >= 0:
+            sound_txtp.write(f"\n# BPM: {sound_bpm}")
+        sound_txtp.write(f"\n")
+        sound_txtp.write(path.relpath(sound_path, start=out_dir) + " " + commands)
---
+++ snap_to_bpm_lut.py
@@ -0,0 +1,6 @@
+# Ending silence will not be stripped if "BPM" is set to -100.
+
+SNAP_TO_BGM_LUT = {
+    R"E:\joshw\ut_dusttale_sp\act1\02. 弄好事情 (Set something right) [BV1Cr421M7pA].m4a": 53,
+    R"E:\joshw\ut_dusttale_sp\act1\15. 瞎操心，结果尴尬了 (Awkworried) (v2) [BV1zr421M7U6].m4a": -100
+}
