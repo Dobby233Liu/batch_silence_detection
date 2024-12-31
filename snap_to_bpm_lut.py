8de93078db0bff5c271bc5b7badc75ba97a1e596
Liu Wenyuan <15816141883@163.com>
2024-12-31 23:39:30 +0800

    More prints, fix beat snapping

--- snap_to_bpm_lut.py
+++ snap_to_bpm_lut.py
@@ -2,5 +2,8 @@
 
 SNAP_TO_BGM_LUT = {
     R"E:\joshw\ut_dusttale_sp\act1\02. 弄好事情 (Set something right) [BV1Cr421M7pA].m4a": 53,
-    R"E:\joshw\ut_dusttale_sp\act1\15. 瞎操心，结果尴尬了 (Awkworried) (v2) [BV1zr421M7U6].m4a": -100
+    R"E:\joshw\ut_dusttale_sp\act1\15. 瞎操心，结果尴尬了 (Awkworried) (v2) [BV1zr421M7U6].m4a": -100,
+    R"E:\joshw\ut_dusttale_sp\act1\36a. 归冢 (Vania) [BV14YWBeMErw].m4a": 96,
+    R"E:\joshw\ut_dusttale_sp\act1\38. 西瓜君-晓宇, sprins anyways - 活该 (Deserve this) [BV1GJ4m1n71X].m4a": 64.6,
+    R"E:\joshw\ut_dusttale_sp\act1\40. 结束了，无论如何 (It's over, anyways) (with The Final Side B) [BV1Lx4y147Xi].m4a": -100,
 }
