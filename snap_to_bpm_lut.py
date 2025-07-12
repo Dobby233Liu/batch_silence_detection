10110a2683b7e48ace20f3511a21c70a2d40efe9
Liu Wenyuan <15816141883@163.com>
2025-07-12 13:35:45 +0800

    Update LUT to reflect set changes

--- snap_to_bpm_lut.py
+++ snap_to_bpm_lut.py
@@ -1,14 +1,14 @@
 # Ending silence will not be stripped if "BPM" is set to -100.
 
 SNAP_TO_BGM_LUT = {
-    R"E:\joshw\ut_dusttale_sp\act1\02. 弄好事情 (Set something right) [BV1Cr421M7pA].m4a": 53,
-    R"E:\joshw\ut_dusttale_sp\act1\03. 你畏惧了的朋友 (Your scared friend) [BV1QK421k7jG].m4a": -100,
-    R"E:\joshw\ut_dusttale_sp\act1\04. 敌军士气低落 (Enemies' low morale) (v2) [BV15f421i73M].m4a": (91.88, True),
-    #R"E:\joshw\ut_dusttale_sp\act1\07. 不归路 (No way back) [BV15A4m1G7na].m4a": 61.25,
-    R"E:\joshw\ut_dusttale_sp\act1\12. 惑之不解 (Perplexe) [BV1WW421N7Su].m4a": (145.066, True),
-    R"E:\joshw\ut_dusttale_sp\act1\15. 瞎操心，结果尴尬了 (Awkworried) (v2) [BV1zr421M7U6].m4a": -100,
-    R"E:\joshw\ut_dusttale_sp\act1\35a. 尸遗 (Corpsolate) (v1) [BV1Di421h7be].m4a": 86.15,
-    #R"E:\joshw\ut_dusttale_sp\act1\36a. 归冢 (Vania) [BV14YWBeMErw].m4a": 96,
-    R"E:\joshw\ut_dusttale_sp\act1\38. 西瓜君-晓宇, sprins anyways - 活该 (Deserve this) [BV1GJ4m1n71X].m4a": 64.6,
-    R"E:\joshw\ut_dusttale_sp\act1\40. 结束了，无论如何 (It's over, anyways) (with The Final Side B) [BV1Lx4y147Xi].m4a": -100,
+    R"E:\joshw\ut_dusttale_sp\act1\02. 弄好事情 (Set something right).mp3": 53,
+    R"E:\joshw\ut_dusttale_sp\act1\03. 你畏惧了的朋友 (Your scared friend).mp3": -100,
+    R"E:\joshw\ut_dusttale_sp\act1\04. 敌军士气低落 (Enemies' low morale) (v2).mp3": (91.88, True),
+    #R"E:\joshw\ut_dusttale_sp\act1\07. 不归路 (No way back).mp3": 61.25,
+    R"E:\joshw\ut_dusttale_sp\act1\12. 惑之不解 (Perplexe).mp3": (145.066, True),
+    R"E:\joshw\ut_dusttale_sp\act1\15. 瞎操心，结果尴尬了 (Awkworried) (v2).mp3": -100,
+    R"E:\joshw\ut_dusttale_sp\act1\35. 尸遗 (Corpsolate) (v1).mp3": 86.15,
+    #R"E:\joshw\ut_dusttale_sp\act1\36a. 归冢 (Vania).mp3": 96,
+    R"E:\joshw\ut_dusttale_sp\act1\38. 西瓜君-晓宇, sprins anyways - 活该 (Deserve this).mp3": 64.6,
+    R"E:\joshw\ut_dusttale_sp\act1\40. 结束了，无论如何 (It's over, anyways) (with The Final Side B).mp3": -100,
 }
